c2f745ca59c12648d7b904a769c57714206a20c2
Andrey Damintsev <damintsew@gmail.com>
2015-09-13 23:48:10 +0300

    Added name po runner

--- ScheduleGitService.sh
+++ ScheduleGitService.sh
@@ -2,6 +2,7 @@
 
 GIT_URL=$1
 BASE_PATH=$2
+APPLICATION_NAME=$(basename $GIT_URL)
 
 LOCAL_DIR=$(pwd)
 
@@ -11,7 +12,7 @@ cd $BASE_PATH
 echo Cloninng repo $GIT_URL
 git clone $1
 
-COMMAND="$LOCAL_DIR/bin/GitServise.sh $1 $2 >> $LOCAL_DIR/log_executor.log 2>&1"
+COMMAND="$LOCAL_DIR/bin/$(APPLICATION_NAME.sh) $1 $2 >> $LOCAL_DIR/log_executor.log 2>&1"
 SCHEDULE="*/1 * * * * root"
 
 echo "$SCHEDULE" $COMMAND
