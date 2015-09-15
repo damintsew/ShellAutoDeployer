73d6b3b216fced15772384fd88efd733bc7e1076
Andrey Damintsev <damintsew@gmail.com>
2015-09-15 22:58:33 +0300

    fix

--- bin/ProgrammRestartService.sh
+++ bin/ProgrammRestartService.sh
@@ -11,12 +11,14 @@ FindConfigFile() {
 
 	if [ -z $CONFIG_FILE ]; then
 		echo "Config file not found!"
-		return [-1]
+		return 1
 	fi
 	
     echo Founded config file: $CONFIG_FILE
 	echo Read config file $(cat $CONFIG_FILE)
-    source $CONFIG_FILE        
+    source $CONFIG_FILE      
+
+	return 0
 }
 
 RunNodeApplication() {
@@ -29,7 +31,7 @@ RestartApplication() {
 	FindConfigFile
 	functionResult=$?
 	
-	if [ $functionResult -eq -1 ]; then
+	if [ $functionResult -eq 1 ]; then
 		return -1
 	fi
 	
